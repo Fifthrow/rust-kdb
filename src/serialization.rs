72a0b4b0635a9af0814413be1de9898a5d908827
Richard Matheson <richard@fifthrowtech.com>
2021-02-08 16:45:53 +0000

    fix: serialization type fixes and tests

--- src/serialization.rs
+++ src/serialization.rs
@@ -16,7 +16,7 @@ pub enum SerializationMode {
 
 /// Serialize a K object using KDB serialization.
 #[inline]
-pub fn b9_serialize(mode: SerializationMode, k: impl AsRef<Any>) -> Result<KBox<Any>, KBox<KError>> {
+pub fn b9_serialize(mode: SerializationMode, k: impl AsRef<Any>) -> Result<KBox<List<u8>>, KBox<KError>> {
     b9_serialize_any(mode, k.as_ref())
 }
 
@@ -29,7 +29,7 @@ unsafe fn wrap_ee<T: KObject>(k: *mut K) -> Result<KBox<T>, KBox<KError>> {
     }
 }
 
-fn b9_serialize_any(mode: SerializationMode, k: &Any) -> Result<KBox<Any>, KBox<KError>> {
+fn b9_serialize_any(mode: SerializationMode, k: &Any) -> Result<KBox<List<u8>>, KBox<KError>> {
     unsafe { wrap_ee(kapi::b9(mode as i32, k.k_ptr())) }
 }
 
@@ -37,4 +37,33 @@ fn b9_serialize_any(mode: SerializationMode, k: &Any) -> Result<KBox<Any>, KBox<
 #[inline]
 pub fn d9_deserialize(k: impl AsRef<List<u8>>) -> Result<KBox<Any>, KBox<KError>> {
     unsafe { wrap_ee(kapi::d9(k.as_ref().k_ptr() as *mut _) as *mut _) }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::convert::TryInto;
+
+    use super::*;
+    use crate::list;
+
+    #[test]
+    fn b9_d9_roundtrips() {
+        let l = list![i32; 1, 2, 3];
+
+        let bytes = b9_serialize(SerializationMode::InProc, l.as_ref()).unwrap();
+        let v: KBox<List<i32>> = d9_deserialize(bytes).unwrap().try_into().unwrap();
+        assert_eq!(v.as_slice(), &[1, 2, 3]);
+
+        let bytes = b9_serialize(SerializationMode::Enumerate, l.as_ref()).unwrap();
+        let v: KBox<List<i32>> = d9_deserialize(bytes).unwrap().try_into().unwrap();
+        assert_eq!(v.as_slice(), &[1, 2, 3]);
+
+        let bytes = b9_serialize(SerializationMode::Unenumerate, l.as_ref()).unwrap();
+        let v: KBox<List<i32>> = d9_deserialize(bytes).unwrap().try_into().unwrap();
+        assert_eq!(v.as_slice(), &[1, 2, 3]);
+
+        let bytes = b9_serialize(SerializationMode::Compress, l.as_ref()).unwrap();
+        let v: KBox<List<i32>> = d9_deserialize(bytes).unwrap().try_into().unwrap();
+        assert_eq!(v.as_slice(), &[1, 2, 3]);
+    }
 }

78e03c39fd029cbccf817c1179661ea6bae74aeb
Richard Matheson <richard@fifthrowtech.com>
2021-02-08 16:46:36 +0000

    example: publishing

--- examples/publishing.rs
+++ examples/publishing.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use kdb::{list, symbol, Connection};
+
+/// Note you'll need a kdb instance on localhost:4200 with the function "upd" defined for this to work...
+fn main() {
+    let conn = Connection::connect("127.0.0.1", 4200, "", None).unwrap();
+    let l = list![i32; 1, 2, 3];
+    if let Err(err) = conn.publish("upd", symbol("some_topic"), l) {
+        println!("Publishing failed: {}", err);
+    }
+}

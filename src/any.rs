5d3a2ee33460171eb26f12a51352c61367711777
Richard Matheson <richard@fifthrowtech.com>
2021-02-14 21:04:53 +0000

    feat: add `try_cast!` macro to complement `cast!`

--- src/any.rs
+++ src/any.rs
@@ -194,6 +194,42 @@ impl From<KBox<Table>> for KBox<Any> {
     }
 }
 
+impl TryFrom<&Any> for &KError {
+    type Error = ConversionError;
+
+    fn try_from(any: &Any) -> Result<Self, Self::Error> {
+        if any.k.t == ERROR {
+            Ok(unsafe { &*(any as *const _ as *const _) })
+        } else {
+            Err(ConversionError::InvalidKCast {
+                from: any.k.t,
+                to: ERROR,
+            })
+        }
+    }
+}
+
+impl TryFrom<KBox<Any>> for KBox<KError> {
+    type Error = ConversionError;
+
+    fn try_from(any: KBox<Any>) -> Result<Self, Self::Error> {
+        if any.as_ref().k.t == ERROR {
+            Ok(unsafe { mem::transmute(any) })
+        } else {
+            Err(ConversionError::InvalidKCast {
+                from: any.as_ref().k.t,
+                to: ERROR,
+            })
+        }
+    }
+}
+
+impl From<KBox<KError>> for KBox<Any> {
+    fn from(value: KBox<KError>) -> Self {
+        unsafe { mem::transmute(value) }
+    }
+}
+
 impl<T: KListable> From<KBox<List<T>>> for KBox<Any> {
     fn from(value: KBox<List<T>>) -> Self {
         unsafe { mem::transmute(value) }
@@ -237,45 +273,76 @@ impl KListable for Any {
 #[doc(hidden)]
 pub trait KdbCast<T: Sized> {
     type Output;
-    fn cast(self) -> Self::Output;
+    fn try_cast(self) -> Result<Self::Output, ConversionError>;
 }
 
 impl<T: KObject + KTyped> KdbCast<T> for KBox<Any> {
     type Output = KBox<T>;
-    fn cast(self) -> KBox<T> {
+    fn try_cast(self) -> Result<Self::Output, ConversionError> {
         unsafe {
             if (*self.k_ptr()).t != T::K_TYPE {
-                panic!("Invalid KDB cast from {} to {}", (*self.k_ptr()).t, T::K_TYPE);
+                Err(ConversionError::InvalidKCast {
+                    from: (*self.k_ptr()).t,
+                    to: T::K_TYPE,
+                })
+            } else {
+                #[allow(clippy::clippy::transmute_ptr_to_ptr)]
+                Ok(mem::transmute(self))
             }
-            mem::transmute(self)
         }
     }
 }
 
 impl<'a, T: 'a + KObject + KTyped> KdbCast<T> for &'a KBox<Any> {
     type Output = &'a KBox<T>;
-    fn cast(self) -> &'a KBox<T> {
+    fn try_cast(self) -> Result<Self::Output, ConversionError> {
         unsafe {
             if (*self.k_ptr()).t != T::K_TYPE {
-                panic!("Invalid KDB cast from {} to {}", (*self.k_ptr()).t, T::K_TYPE);
+                Err(ConversionError::InvalidKCast {
+                    from: (*self.k_ptr()).t,
+                    to: T::K_TYPE,
+                })
+            } else {
+                #[allow(clippy::clippy::transmute_ptr_to_ptr)]
+                Ok(mem::transmute(self))
             }
-            #[allow(clippy::clippy::transmute_ptr_to_ptr)]
-            mem::transmute(self)
         }
     }
 }
 
 impl<'a, T: 'a + KObject + KTyped> KdbCast<T> for &'a Any {
     type Output = &'a T;
-    fn cast(self) -> &'a T {
+    fn try_cast(self) -> Result<Self::Output, ConversionError> {
         unsafe {
             if (*self.k_ptr()).t != T::K_TYPE {
-                panic!("Invalid KDB cast from {} to {}", (*self.k_ptr()).t, T::K_TYPE);
+                Err(ConversionError::InvalidKCast {
+                    from: (*self.k_ptr()).t,
+                    to: T::K_TYPE,
+                })
+            } else {
+                #[allow(clippy::clippy::transmute_ptr_to_ptr)]
+                Ok(mem::transmute(self))
             }
-            #[allow(clippy::clippy::transmute_ptr_to_ptr)]
-            mem::transmute(self)
         }
     }
+}
+
+/// Tries to cast a KBox<Any>, &KBox<Any> or &Any to another KDB type.
+///
+/// returns a result containing the KDB type, or a `ConversionError`.
+///
+/// # Example
+///
+/// ```
+/// # use kdb::{try_cast, symbol, Any, Atom, KBox, Symbol};
+/// let a: KBox<Any> = symbol("Hello").into();
+/// assert_eq!(symbol("Hello"), try_cast!(a; Atom<Symbol>).unwrap().value());
+/// ```
+#[macro_export]
+macro_rules! try_cast {
+    ($ex:expr; $t:ty) => {
+        kdb::KdbCast::<$t>::try_cast($ex)
+    };
 }
 
 /// Cast a KBox<Any>, &KBox<Any> or &Any to another KDB type.
@@ -292,6 +359,6 @@ impl<'a, T: 'a + KObject + KTyped> KdbCast<T> for &'a Any {
 #[macro_export]
 macro_rules! cast {
     ($ex:expr; $t:ty) => {
-        kdb::KdbCast::<$t>::cast($ex)
+        kdb::KdbCast::<$t>::try_cast($ex).unwrap()
     };
 }
